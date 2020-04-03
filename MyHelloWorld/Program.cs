ffb30dbeab661bf8bd25bd285227295e4b3f9046
René van Dorp <dorpr@wigo4it.nl>
2020-04-03 12:36:27 +0200

    Added Hello Rene

--- MyHelloWorld/Program.cs
+++ MyHelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace MyHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Rene!");
         }
     }
 }
