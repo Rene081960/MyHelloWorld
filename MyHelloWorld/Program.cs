5744b35c6f7fbd214015522c97c044edd46b6f32
René van Dorp <dorpr@wigo4it.nl>
2020-04-03 12:33:18 +0200

    Add project files.

---
+++ MyHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
